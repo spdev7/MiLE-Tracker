2096f4d72f9885f453310f07d5d8a25643b51a49
Saurav Prakash <sauravprakash7@outlook.com>
2017-12-30 21:18:09 +0530

    Basic Layout

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).one('pageinit',function(){
+  //Add Handler
+  $('#sumbitAdd').on('tap',addRun);
+
+   /*
+    * Add a run
+
+    */
+    function addRun(){
+      alert(1);
+    }
+
+});
