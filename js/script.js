a31743b1a14c8227b592060c79d4d0a87a8c38fa
Saurav Prakash <sauravprakash7@outlook.com>
2017-12-30 21:53:46 +0530

    updating local storage

--- js/script.js
+++ js/script.js
@@ -4,10 +4,51 @@ $(document).one('pageinit',function(){
 
    /*
     * Add a run
-
     */
     function addRun(){
-      alert(1);
+      // Get form values
+       var miles = $('#addMiles').val();
+       var date = $('#addDate').val();
+
+       //Create 'run'  object
+       vaar run = {
+         date: date,
+         miles: parseFloat(miles)
+       };
+
+      var runs = getRunsObject();
+      //Add run to runs Array
+      runs.push(run);
+
+      alert('Run Added');
+
+      //Set stringified object to localStorage
+      localstorage.setItem('runs', JSON.stringify(runs));
+
+     //Redirect
+     window.location.href="index.html";
+
+     return false;
+
     }
+    /*
+     * Get the runsobject
+     */
+     function addRunObject(){
+           //Set runs array
+           var runs = new Array();
+          // Get current runs from local storage
+          var currentRuns = localStorage.getItem('runs');
+
+          // Check localStorage
+          if(currentRuns != null){
+            //Set to runs
+               var runs = JSON.parse(currentRuns);
+
+          }
+
+             // Return runs object
+             return runs.sort(function(a,b){return new Date(b.date) - new Date(a.date)})
+     }
 
 });
